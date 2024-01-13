1e1491ebcc8a2e66d19e224d6130c3ebb4a34197
0xAdiyat <0xAdiyat@gmail.com>
2024-01-13 18:47:37 +0600

    Initial Commit

---
+++ SwiftUI-Bootcamp/SwiftUI_BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BootcampApp.swift
+//  SwiftUI-Bootcamp
+//
+//  Created by Ahsaf Hussain Adiyat on 13/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
